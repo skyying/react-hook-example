172ba2d7454385d26038812e012c594b9e275ae3
Chien Wen Liu <dreamuk@gmail.com>
2019-02-10 22:11:01 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import "./style/main.scss";
 import React from "react";
 import ReactDOM from "react-dom";
-import { FunctionalCompInput } from "./components/FunctionalCompInput.js";
+import {FunctionalCompInput} from "./components/FunctionalCompInput.js";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <FunctionalCompInput />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById("main"));
+ReactDOM.render(<FunctionalCompInput />, document.getElementById("main"));
